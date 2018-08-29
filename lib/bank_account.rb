cdd824f72c750bf2405f12db1d99138bab2a61a5
github username <github email address>
2018-08-29 18:55:46 -0400

    Paired programming

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,31 @@
 class BankAccount
+  attr_accessor :balance, :status
+  attr_reader :name
 
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit(amount)
+    @balance += amount
+  end
+
+  def display_balance
+     "Your balance is $#{@balance}."
+  end
+
+  def valid?
+    @balance > 0 && @status == "open" ? true : false
+  end
+
+  def close_account
+    @status = "closed"
+  end
 end
+
+##Instance can transfer something to another Instance
+
+account1 = BankAccount.new("name")
+account2 = BankAccount.new("name2")
