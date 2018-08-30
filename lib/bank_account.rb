0f2584679218073dba048aa30535e1a28e074f12
skarolk <skarolkiewicz@gmail.com>
2018-08-29 22:46:04 -0400

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,39 @@
+require_relative "./transfer.rb"
+
 class BankAccount
+
+  attr_accessor :bank_account, :balance, :status
+  attr_reader :name
+
+  def initialize(name)
+    @bank_account = bank_account
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit(deposit_amount)
+    @balance += deposit_amount
+  end
+
+  def display_balance
+    return "Your balance is $#{@balance}."
+  end
+
+  def valid?
+    if @status == "open" && @balance > 0
+      true
+    else
+      false
+    end
+  end
+
+  def sender
+    @name
+  end
+
+  def close_account
+    @status = "closed"
+  end
 
 end
