1ddd9f683561841bb0e3a9f8a0a1ef60de993fc5
MarSchlaefer <mschlaefer00@gmail.com>
2018-08-30 15:01:10 -0400

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,38 @@
+require 'pry'
 class BankAccount
+
+  attr_accessor :balance, :status, :amount
+  attr_reader :name
+
+  #initializes instance of BankAccount with name, starting balance of 1000, and an open status.
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  #adds an amount of money into @balance
+  def deposit(amount)
+    @balance += amount
+  end
+
+  #returns current balance amount
+  def display_balance
+    "Your balance is $#{@balance}."
+  end
+
+  #returns true if account has open status and has a balance of greater than $0
+  def valid?
+    if @status == "open" && @balance > 0
+      return true
+    else
+      return false
+    end
+  end
+
+  #changes status from open to closed
+  def close_account
+    @status = "closed"
+  end
 
 end
